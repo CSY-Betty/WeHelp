dd05037fdd94797a305f5bff64802004f59cc5a2
Betty <qwe50929@gmail.com>
2023-07-23 15:38:04 +0800

    feat: week3 assignment4

--- stage1/week3/assignment.js
+++ stage1/week3/assignment.js
@@ -1,28 +1,95 @@
-fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function(response){
-    return response.json();
-}).then(function(responseData){
-    data = responseData.result.results;
-    const imageContainer = document.getElementsByClassName("attractionPic");
-    const spanContainer = document.getElementsByClassName("attractionName");
+// 建立照片模板
+function createAndAppendElementPic(tag, className, src, container) {
+    const element = document.createElement(tag);
+    element.classList.add(className);    
+    const img = document.createElement("img");
+    img.src = src;
+    element.appendChild(img);
+    
+    container.appendChild(element)
+}
 
-    Array.from(imageContainer).forEach(function(container, index) {if (index < data.length){
-        const item = data[index];
-        const file = item.file;
-        const file_lower = file.toLowerCase();
+// 建立景點名稱模板
+function createAndAppendElementText(tag, className, title, container) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.textContent = title;
+    container.appendChild(element)
+}
+
+// 處理取得的資料並套入模板
+function processDataAndDisplay(data, isProduct){
+    data.forEach(function(item) {
+        const {file, stitle} = item;
+        // 取得圖片的第一個網址
+        let file_lower = file.toLowerCase();
         const image = file_lower.indexOf(".jpg");
         const first_image = file.substring(0, image + 4);
+
+        // 建立最外層區塊
+        if (isProduct) {
+            const container = document.getElementById("product-container")
+            const productItem = document.createElement("div");
+            productItem.classList.add("product-item");
+
+            createAndAppendElementPic("div", "attractionPic", first_image, productItem)
+            createAndAppendElementText("div", "attractionName", stitle, productItem)
+
+            container.appendChild(productItem)
+        }
+        else {
+            const container = document.getElementById("attractions-container")
+            const titleItem = document.createElement("div");
+            titleItem.classList.add("title-item");
+
+            createAndAppendElementPic("div", "attractionPic", first_image, titleItem)
+            createAndAppendElementPic("div", "star", "./star.png", titleItem)
+            createAndAppendElementText("div", "attractionName", stitle, titleItem)
+            
+
+            container.appendChild(titleItem);
+        }
         
-        const newImage = document.createElement("img");
-        newImage.src = first_image;
-        container.appendChild(newImage);
-    }});
+    })
 
-    Array.from(spanContainer).forEach(function(container, index) {if (index < data.length){
-        const item = data[index];
-        const stitle = item.stitle;
+}
 
-        const attractionName = document.createElement("span");
-        attractionName.textContent = stitle;
-        container.appendChild(attractionName);
-    }});
-}); 
+
+// 爬取資料
+fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json")
+    .then(response => response.json())
+    .then(function(responseData) {
+        const productData = responseData.result.results.slice(0,3);
+        const articleData = responseData.result.results.slice(3,15);
+        processDataAndDisplay(productData, true);
+        processDataAndDisplay(articleData, false);
+    })
+    
+
+// 設定load more按鈕
+let currentIndex = 15;
+const totalAttractions = 58;
+const loadMoreButton = document.getElementById("load-more");
+loadMoreButton.addEventListener("click", function() {
+    fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json")
+    .then(response => response.json())
+    .then(function(responseData) {
+        data = responseData.result.results;
+        const endIndex = Math.min(currentIndex + 12, totalAttractions);
+        articleData = data.slice(currentIndex, endIndex);
+    
+        if (articleData.length >= 12) {
+            processDataAndDisplay(articleData, false);
+            currentIndex = endIndex;
+
+            if (currentIndex === totalAttractions) {
+                loadMoreButton.disabled = true;
+            }
+        }
+        else {
+            const articleData = data.slice(currentIndex);
+            processDataAndDisplay(articleData, false);
+            loadMoreButton.disabled = true;
+        }
+    })
+})
